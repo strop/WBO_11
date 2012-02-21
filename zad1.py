6abce3863403a0c3bc0fa678e321e98e58dd3d84
J <a@a.pl>
2012-02-21 20:37:07 +0100

    Obsłużony przypadek braku parametru k

--- zad1.py
+++ zad1.py
@@ -3,16 +3,33 @@ from Bio import SeqIO
 def gen_substrings(s, l):
   return [s[x:x+l] for x in range(0,len(s)-l+1)]
 
-def gen_probes(fname = "test.fa", k = -1):
+def gen_probes_inner(sequences, k):
   substrings = []
-  sequences = [ x for x in SeqIO.parse(fname, "fasta")]
   uniques = []
   seq_count = len(sequences)
   for x in sequences:
-    substrings.append(set(gen_substrings(x.seq, k)))
+    substrings.append(gen_substrings(x.seq, k))
   for i in range(seq_count):
     uniques.append(substrings[i])
-    for j in [j for j in range(0,seq_count) if j != i]:
-      uniques[i] = uniques[i] - substrings[j];
-  for i in range(seq_count):
-    print uniques[i].pop().reverse_complement()
+    for j in range(seq_count):
+      if j != i:
+        uniques[i] = [x for x in uniques[i] if sequences[j].seq.find(x) == -1]
+  probes = []
+  try:
+    for i in range(seq_count):
+      probes.append(uniques[i][0])
+    return probes
+  except IndexError:
+    return None
+
+def gen_probes(fname = "test.fa", k = -1):
+  sequences = [ x for x in SeqIO.parse(fname, "fasta")]
+  probes = None
+  if k == -1:
+    k = 1
+    while probes is None:
+      probes = gen_probes_inner(sequences, k)
+      k += 1
+  else:
+    probes = gen_probes_inner(sequences, k)
+  return probes
