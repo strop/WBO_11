60440c59954dec044d7afbdd23b8d843fb489193
J <a@a.pl>
2012-03-04 21:57:00 +0100

    Struktura katalogow, zad2

--- 1zad/wbo-1-jl291673.py
+++ 1zad/wbo-1-jl291673.py
@@ -1,13 +1,31 @@
+# WBO 11/12 - Zad 1
+# Autor: Jarek Łukow (291673)
+
 from Bio import SeqIO
-# funkcja pomocnicza generujaca podciagi dlugosci l
+
+# Uzycie:
+# glowna funkcja to gen_probes(fileHandle, k, filtr)
+# fileHandle powinien byc uchwytem do pliku (otrzymanym np. przez open("test.fa"))
+# k okresla dlugosc wszystkich sekwencji w rozwiazaniu
+# filtr to funkcja Seq -> Bool, ktora zwraca True wtw podana w parametrze sekwencja moze byc uzyta w rozwiazaniu
+# Funkcja rzuca wyjatek w wypadku braku rozwiazan
+#
+# parametry k i filtr sa opcjonalne:
+# jesli podano tylko k, funkcja zwroci sekwencje dlugosci dokladnie k
+# jesli nie podano k, funkcja bedzie szukac rozwiazan dla kolejnych k zaczynajac od 1 i zwroci pierwsze znalezione rozwiazanie
+# jesli podano filtr i k, to funkcja zwroci sekwencje dlugosci dokladnie k, ktore zostaly zaakceptowane przez filtr
+# jesli podano filtr, ale nie podano k, funkcja zwroci rozwiazania wyszukane sposrod wszystkich podslow podanych sekwencji (to znaczy wszystkie sekwencje w rozwiazaniu beda akceptowane przez filtr, ale byc moze nie wszystkie beda tej samej dlugosci)
+
+# funkcja pomocnicza generujaca podslowa dlugosci l
 def gen_substrings(s, length):
   return [s[x:x+length] for x in range(0,len(s)-length+1)]
 
-# funkcja pomocnicza generujaca wszystkie podciagi
+# funkcja pomocnicza generujaca wszystkie podslowa
 def gen_all_substrings(s, dummyParam):
   return [ s[x:x+l] for x in range(len(s)) for l in range(1, len(s)+1) if x+l < len(s)+1 ]
 
-# funkcja pomocnicza generujaca rozwiazania dlugosci k
+# funkcja pomocnicza generujaca rozwiazania, mniej czula na brak parametrow
+# generator podslow jest parametrem, bo czasem chcemy miec wszystkie podslowa, a czasem tylko te okreslonej dlugosci
 def gen_probes_inner(sequences, k, filtr, substrings_generator):
   substrings = []
   uniques = []
@@ -16,7 +34,7 @@ def gen_probes_inner(sequences, k, filtr, substrings_generator):
     uniques.append([x for x in substrings_generator(sequences[i].seq, k) if filtr(x)])
     for j in range(seq_count):
       if j != i:
-        uniques[i] = [x for x in uniques[i] if sequences[j].seq.find(x) == -1]
+        uniques[i] = [x for x in uniques[i] if sequences[j].seq.find(x) == -1] # eliminujemy fragmenty ktore sa tez podslowami w innej sekwencji
   probes = []
   for i in range(seq_count):
     if len(uniques[i]) == 0: # jakas sekwencja nie posiada unikalnego fragmentu dlugosci k
@@ -30,14 +48,13 @@ def gen_probes(fileHandle, k = None, filtr = None):
   defFiltr = lambda(x): True
   probes = None
   if k is None:
-    # wyznaczamy dlugosc najkrotszej sekwencji, zeby wiedziec kiedy zakonczyc przeszukiwanie przy nieokreslonym k
-    minLen = len(sequences[0])
-    for i in range(len(sequences)):
-      if len(sequences[i]) < minLen:
-        minLen = len(sequences[i])
-    print "minLen: ", minLen
     if (filtr is None) or (type(filtr) != type(defFiltr)): # nie mamy ani k ani filtra, wiec szukamy rozwiazania dla najmniejszego k
       filtr = defFiltr
+      # wyznaczamy dlugosc najkrotszej sekwencji, zeby wiedziec kiedy zakonczyc przeszukiwanie przy nieokreslonym k
+      minLen = len(sequences[0])
+      for i in range(len(sequences)):
+        if len(sequences[i]) < minLen:
+          minLen = len(sequences[i])
       k = 1
       # poszukujemy coraz dluzszych rozwiazan
       while (probes is None) and (k < minLen):
