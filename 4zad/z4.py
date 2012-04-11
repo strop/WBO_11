9ed2088881c67c314f0462e42db968ada75a2226
J <a@a.pl>
2012-04-11 11:22:10 +0200

    Viterbi się nie wysypuje

--- 4zad/z4.py
+++ 4zad/z4.py
@@ -64,19 +64,50 @@ def normalize_freqs(freqs, count):
 def load_freqs(filename):
   return pickle.load(open(filename, 'r'))
 
+def fold_and_normalize(tree, depth, count):
+  tree = fold_prefix_tree(tree, depth)
+  return normalize_freqs(tree, count)
+
+def build_markov(plain_freqs, promo_freqs):
+  plain_promo_prob = 0.1
+  promo_plain_prob = 0.1
+
+  g_state = 'g'
+  p_state='p'
+  states = Alphabet.Alphabet()
+  states.letters = [g_state, p_state]
+
+  symbols = Alphabet.Alphabet()
+  symbols.letters = plain_freqs.keys()
+
+  b = MM.MarkovModelBuilder(states, symbols)
+  #b.set_initial_probabilities({g_state : 0.5, p_state : 0.5})
+  b.allow_all_transitions()
+  b.set_equal_probabilities()
+  b.set_transition_score(g_state, p_state, plain_promo_prob)
+  b.set_transition_score(g_state, g_state, 1 - plain_promo_prob)
+  b.set_transition_score(p_state, g_state, promo_plain_prob)
+  b.set_transition_score(p_state, p_state, 1 - promo_plain_prob)
+  for (s, p) in plain_freqs.items():
+    b.set_emission_score(g_state, s, p)
+  for (s, p) in promo_freqs.items():
+    b.set_emission_score(p_state, s, p)
+  return (b.get_markov_model(), states) 
+
 ''' Na podstawie danych plików z sekwencjami promotorowymi i danego genomu funkcja buduje model Markova'''
 def main_build_Markov(promotor_filename = "promotor.fa", genome_filename = "genom.fa"):
   pass 
 
 ''' Na podstawie danego modelu Markova funkcja wyznacza prawdopodobne fragmenty promotorowe w danym genomie '''
-def main_get_promotor_frags(markov_model, genome_filename = "genom.fa"):
+def main_get_promotor_frags(markov_model, states, symbol_length = 2, genome_filename = "genom.fa"):
+  genome = [ x for x in SeqIO.parse(genome_filename, "fasta")]
+  genome = genome[0]
+  prediction = markov_model.viterbi(genome, states)
+  genome_splitted = [0] * (len(genome[0])/symbol_length)
+  for i in range(0, len(genome)-1, symbol_length):
+    genome_splitted[i/symbol_length] = (genome[i] + genome[i+1])
+  viterbi = markov.viterbi(genome_splitted, states)
   pass
-
-def fold_and_normalize(tree, depth, count):
-  tree = fold_prefix_tree(tree, depth)
-  return normalize_freqs(tree, count)
-
-
 
 promotor_sequences = [ x for x in SeqIO.parse("promotor.fa", "fasta")]
 genome = [ x for x in SeqIO.parse("genom.fa", "fasta")]
@@ -96,33 +127,30 @@ promotor_freqs = fold_and_normalize(promotor_freqs[symbol_length], symbol_length
 
 genome_freqs = fold_and_normalize(genome_freqs[symbol_length], symbol_length, genome_counts[symbol_length])
 
-def build_markov(plain_freqs, promo_freqs):
-  plain_promo_prob = 0.1
-  promo_plain_prob = 0.1
+for k in promotor_freqs:
+  assert(k in genome_freqs)
+for k in genome_freqs:
+  assert(k in genome_freqs)
 
-  states = Alphabet.Alphabet()
-  states.letters = ["PLAIN", "PROMO"]
+print promotor_freqs
 
-  symbols = Alphabet.Alphabet()
-  symbols.letters = plain_freqs.keys()
+(markov, states) = build_markov(genome_freqs, promotor_freqs)
 
-  b = MM.MarkovModelBuilder(states, symbols)
-  #b.set_initial_probabilities({"PLAIN" : 0.5, "PROMO" : 0.5})
-  b.allow_all_transitions()
-  b.set_equal_probabilities()
-  b.set_transition_score("PLAIN", "PROMO", plain_promo_prob)
-  b.set_transition_score("PLAIN", "PLAIN", 1 - plain_promo_prob)
-  b.set_transition_score("PROMO", "PLAIN", promo_plain_prob)
-  b.set_transition_score("PROMO", "PROMO", 1 - promo_plain_prob)
-  for (s, p) in plain_freqs.items():
-    b.set_emission_score("PLAIN", s, p)
-  for (s, p) in promo_freqs.items():
-    b.set_emission_score("PROMO", s, p)
-  return (b.get_markov_model(), states, symbols) 
+# Fragmenty odpowiedzialne za tłumaczenie wieloliterowych symboli emisji na symbole jednoznakowe
+# okazały się niepotrzebne, bo Viterbi nie działał z powodu wieloznakowych nazw *stanów* a nie symboli
 
-(markov, states, symbols) = build_markov(genome_freqs, promotor_freqs)
-genome_splitted = [0] * (len(genome[0])/2)
-for i in range(0, len(genome[0])-1, 2):
-  genome_splitted[i/2] = (genome[0][i] + genome[0][i+1])
-genome[0].alphabet = symbols
-markov.viterbi(genome_splitted, states)
+#def translate_symbols(freqs, translation):
+#  assert(len(freqs) <= len(translation))
+#  for k in freqs.keys():
+#    freqs[translation[k]] = freqs[k]
+#    del freqs[k]
+
+#single_char_symbols = map(chr, range(ord('a'), ord('z')+1))
+#single_char_trans = {}
+#for (k, t) in zip(genome_freqs.keys(), single_char_symbols):
+#  single_char_trans[k] = t
+
+#translate_symbols(promotor_freqs, single_char_trans)
+#translate_symbols(genome_freqs, single_char_trans)
+#for i in range(len(genome_splitted)):
+#  genome_splitted[i] = single_char_trans[genome_splitted[i]]
