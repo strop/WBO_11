6a905a71733b3d65c3ee2ae799dfccabe5a3d044
J <a@a.pl>
2012-04-21 21:58:56 +0200

    z4 finalizacja

--- 4zad/z4.py
+++ 4zad/z4.py
@@ -8,9 +8,10 @@ from copy import deepcopy
 class Dumpfiles:
   promotor_freq = "promotor_freq.dump"
   genome_freq = "genome_freq.dump"
+  viterbi_result = "viterbi.dump"
 
-''' Tworzy drzewko prefiksowe, które posłuży do zliczania podsłów sekwencji. (Użycie pythonowego substringa s[1:4] zużywa mnóstwo pamięci) '''
 def prefix_tree(depth, initial_value = 0, symbols = ['A','T','G','C']):
+  ''' Tworzy drzewko prefiksowe, które posłuży do zliczania podsłów sekwencji. (Użycie pythonowego substringa s[1:4] zużywa mnóstwo pamięci) '''
   symbol_tree = {}
   for s in symbols:
     symbol_tree[s] = initial_value
@@ -21,8 +22,8 @@ def prefix_tree(depth, initial_value = 0, symbols = ['A','T','G','C']):
     symbol_tree = new_root
   return symbol_tree
 
-''' Przywraca drzewko prefiksowe do 'normalnej' postaci - jednopoziomowego słownika <sekwencja> : <częstotliwość> '''
 def fold_prefix_tree(tree, depth):
+  ''' Przywraca drzewko prefiksowe do 'normalnej' postaci - jednopoziomowego słownika <sekwencja> : <częstotliwość> '''
   for d in range(depth-1):
     tree2 = {}
     for k1 in tree.keys():
@@ -33,7 +34,7 @@ def fold_prefix_tree(tree, depth):
 
 symbol_lengths = [1,2,3,4]
 
-def calc_symbol_freq(seqs, dump_filename):
+def calc_symbol_freq(seqs):
   freqs = [0,0,0,0]
   for l in range(1,4):
     symbol_tree = prefix_tree(l)
@@ -46,8 +47,6 @@ def calc_symbol_freq(seqs, dump_filename):
 	  curr_dict = curr_dict[s[j]]
         curr_dict[s[i+l-1]] = curr_dict[s[i+l-1]] + 1
     freqs[l] = symbol_tree
-  dump_file = open(dump_filename, "w")
-  pickle.dump(freqs, dump_file)
   return freqs
 
 def calc_counts(seqs):
@@ -61,8 +60,6 @@ def normalize_freqs(freqs, count):
     freqs[k] = float(freqs[k]) / float(count)
   return freqs
 
-def load_freqs(filename):
-  return pickle.load(open(filename, 'r'))
 
 def fold_and_normalize(tree, depth, count):
   tree = fold_prefix_tree(tree, depth)
@@ -81,7 +78,6 @@ def build_markov(plain_freqs, promo_freqs):
   symbols.letters = plain_freqs.keys()
 
   b = MM.MarkovModelBuilder(states, symbols)
-  #b.set_initial_probabilities({g_state : 0.5, p_state : 0.5})
   b.allow_all_transitions()
   b.set_equal_probabilities()
   b.set_transition_score(g_state, p_state, plain_promo_prob)
@@ -94,63 +90,79 @@ def build_markov(plain_freqs, promo_freqs):
     b.set_emission_score(p_state, s, p)
   return (b.get_markov_model(), states) 
 
-''' Na podstawie danych plików z sekwencjami promotorowymi i danego genomu funkcja buduje model Markova'''
-def main_build_Markov(promotor_filename = "promotor.fa", genome_filename = "genom.fa"):
-  pass 
+def dump_obj(obj, filename):
+  dump_file = open(filename, "w")
+  pickle.dump(obj, dump_file)
 
-''' Na podstawie danego modelu Markova funkcja wyznacza prawdopodobne fragmenty promotorowe w danym genomie '''
-def main_get_promotor_frags(markov_model, states, symbol_length = 2, genome_filename = "genom.fa"):
-  genome = [ x for x in SeqIO.parse(genome_filename, "fasta")]
-  genome = genome[0]
-  prediction = markov_model.viterbi(genome, states)
-  genome_splitted = [0] * (len(genome[0])/symbol_length)
-  for i in range(0, len(genome)-1, symbol_length):
-    genome_splitted[i/symbol_length] = (genome[i] + genome[i+1])
-  viterbi = markov.viterbi(genome_splitted, states)
-  pass
+def load_obj(filename):
+  return pickle.load(open(filename, 'r'))
+  
+def main_build_markov(promotor_filename = "promotor.fa", genome_filename = "genom.fa", symbol_length = 2, load_cached = False, save_cache = True):
+  ''' Na podstawie plików z sekwencjami promotorowymi i genomem funkcja buduje model Markova'''
+  promotor_sequences = [ x for x in SeqIO.parse("promotor.fa", "fasta")]
+  genome = [ x for x in SeqIO.parse("genom.fa", "fasta")]
+  if not load_cached:
+    promotor_freqs = calc_symbol_freq(promotor_sequences)
+    genome_freqs = calc_symbol_freq(genome)
+    if save_cache:
+      dump_obj(promotor_freqs, Dumpfiles.promotor_freq)
+      dump_obj(genome_freqs, Dumpfiles.genome_freq)
+  else:
+    promotor_freqs = load_obj(Dumpfiles.promotor_freq)
+    genome_freqs = load_obj(Dumpfiles.genome_freq)
+  
+  promotor_counts = calc_counts(promotor_sequences)
+  genome_counts = calc_counts(genome)
 
-promotor_sequences = [ x for x in SeqIO.parse("promotor.fa", "fasta")]
-genome = [ x for x in SeqIO.parse("genom.fa", "fasta")]
-#promotor_freqs = calc_symbol_freq(promotor_sequences, Dumpfiles.promotor_freq)
-#genome_freqs = calc_symbol_freq(genome, Dumpfiles.genome_freq)
+  print promotor_counts
 
-promotor_freqs = load_freqs(Dumpfiles.promotor_freq)
-genome_freqs = load_freqs(Dumpfiles.genome_freq)
-promotor_counts = calc_counts(promotor_sequences)
-genome_counts = calc_counts(genome)
+  promotor_freqs = fold_and_normalize(promotor_freqs[symbol_length], symbol_length, promotor_counts[symbol_length])
+  genome_freqs = fold_and_normalize(genome_freqs[symbol_length], symbol_length, genome_counts[symbol_length])
 
-print promotor_counts
+  for k in promotor_freqs:
+    assert(k in genome_freqs)
+  for k in genome_freqs:
+    assert(k in genome_freqs)
 
-symbol_length = 2
+  print promotor_freqs
+  (markov, states) = build_markov(genome_freqs, promotor_freqs)
 
-promotor_freqs = fold_and_normalize(promotor_freqs[symbol_length], symbol_length, promotor_counts[symbol_length])
+  return (markov, states)
 
-genome_freqs = fold_and_normalize(genome_freqs[symbol_length], symbol_length, genome_counts[symbol_length])
-
-for k in promotor_freqs:
-  assert(k in genome_freqs)
-for k in genome_freqs:
-  assert(k in genome_freqs)
-
-print promotor_freqs
-
-(markov, states) = build_markov(genome_freqs, promotor_freqs)
-
-# Fragmenty odpowiedzialne za tłumaczenie wieloliterowych symboli emisji na symbole jednoznakowe
-# okazały się niepotrzebne, bo Viterbi nie działał z powodu wieloznakowych nazw *stanów* a nie symboli
-
-#def translate_symbols(freqs, translation):
-#  assert(len(freqs) <= len(translation))
-#  for k in freqs.keys():
-#    freqs[translation[k]] = freqs[k]
-#    del freqs[k]
-
-#single_char_symbols = map(chr, range(ord('a'), ord('z')+1))
-#single_char_trans = {}
-#for (k, t) in zip(genome_freqs.keys(), single_char_symbols):
-#  single_char_trans[k] = t
-
-#translate_symbols(promotor_freqs, single_char_trans)
-#translate_symbols(genome_freqs, single_char_trans)
-#for i in range(len(genome_splitted)):
-#  genome_splitted[i] = single_char_trans[genome_splitted[i]]
+def main_get_promotor_frags(markov_model, states, symbol_length = 2, genome_filename = "genom.fa", load_cache = False, save_cache = True):
+  ''' Na podstawie danego modelu Markova funkcja wyznacza prawdopodobne fragmenty promotorowe w danym genomie
+      markov_model - bipythonowy obiekt modelu markowa
+      states - alfabet stanów modelu markowa
+      symbol_length - długość fragmentu kodu DNA, który uważamy za jeden "symbol" (1,2 lub 3)
+      genome_filename - plik z genomem w formacie fasta, w którym chcemy wykryć fragmenty promotorowe
+      load_cache - jeśli true, to wynik działania algorytmu viterbiego zostanie załadowany z wczesniej przygotowanego pliku (patrz save_cache)
+      save_cache - jeśli true, to wynik działania algorytmu viterbiego zostanie zapisany do pliku do późniejszego wykorzystania
+      
+      zwraca - listę par (indeks_początku_odcinka_promotorowego, indeks_końca_odcinka_promotorowego) '''
+  if load_cache:
+    viterbi = load_obj(Dumpfiles.viterbi_result)
+  else:
+    genome = [ x for x in SeqIO.parse(genome_filename, "fasta")]
+    genome = genome[0]
+    # obiekt przekazany do viterbiego musi być sekwencją symboli, w tym wypadku symbole sa wieloznakowe, więc trzeba podzielić genom na kawałki odpowiedniej długości
+    genome_splitted = [0] * ((len(genome)/symbol_length))
+    for i in range(0, len(genome)-symbol_length+1, symbol_length):
+      chunk = ''.join([genome[j] for j in range(i,i+symbol_length)])
+      genome_splitted[i/symbol_length] = chunk
+    # właściwe uruchomienie alg. viterbiego
+    viterbi = markov_model.viterbi(genome_splitted, states)
+  if save_cache:
+    dump_obj(viterbi, Dumpfiles.viterbi_result)
+  viterbi_string = viterbi[0]
+  state = viterbi_string[0]
+  promotor_positions = []
+  start = 0
+  # tworzenie listy par indeksów (początek,koniec) fragmentu promotorowego - liniowe przejście po sekwencji stanół zwróconej przez alg. viterbiego
+  for i in range(1,len(viterbi_string)):
+    if viterbi_string[i] != state:
+      print "y"
+      if state == 'p':
+        promotor_positions.append((start,i))
+      state = viterbi_string[i]
+      start = i
+  return promotor_positions
